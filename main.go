4334b3b4ede850d0866eb5239ab574f50e6bce78
Shivani <shivaninaik070@gmail.com>
2024-12-26 03:47:32 +0530

    add frontend url to bypass CORS

--- main.go
+++ main.go
@@ -192,7 +192,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"https://massmailgeneratorfrontend.onrender.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
